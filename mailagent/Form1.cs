2132061d66fed34e17ccd983c105a06b66f7c0b7
OlegBrest <pop457045@mail.ru>
2017-02-22 00:43:54 +0300

    mssges on log

--- mailagent/Form1.cs
+++ mailagent/Form1.cs
@@ -36,47 +36,55 @@ namespace mailagent
                 try
                 {
                     client.Connect(txtServer.Text, Convert.ToInt32(txtPort.Text), SSL_bx.Checked);
-                    txtLog.AppendText (DateTime.Now.ToLongTimeString() + " 36. Server connected.\r\n\r\n");
+                    txtLog.AppendText(DateTime.Now.ToLongTimeString() + " 36. Server connected.\r\n\r\n");
                     txtLog.Update();
                     // Authenticate ourselves towards the server
                     try
                     {
                         client.Authenticate(txtUser.Text, txtPass.Text);
-                        txtLog.AppendText (DateTime.Now.ToLongTimeString() + " 49. Server Authenticated.\r\n\r\n");
+                        txtLog.AppendText(DateTime.Now.ToLongTimeString() + " 49. Server Authenticated.\r\n\r\n");
                         txtLog.Update();
 
                         // Get the number of messages in the inbox
                         List<string> mssgs = client.GetMessageUids();
                         int messageCount = mssgs.Count;
-                        txtLog.AppendText (DateTime.Now.ToLongTimeString() + " 54.Total messages: " + messageCount.ToString() + "\r\n\r\n");
+                        txtLog.AppendText(DateTime.Now.ToLongTimeString() + " 54.Total messages: " + messageCount.ToString() + "\r\n\r\n");
                         txtLog.Update();
 
                         // We want to download all messages
-                        //                 List<OpenPop.Mime.Message> allMessages = new List<OpenPop.Mime.Message>(messageCount);
+                        List<OpenPop.Mime.Message> allMessages = new List<OpenPop.Mime.Message>(messageCount);
 
                         // Messages are numbered in the interval: [1, messageCount]
                         // Ergo: message numbers are 1-based.
                         // Most servers give the latest message the highest number
-                        /*                    for (int i = 10; i > 0; i--)
-                                            {
-                                                allMessages.Add(client.GetMessage(i));
-                                            }
-                                            client.Disconnect();*/
+                        for (int i = messageCount; i > (messageCount-10); i--)
+                        {
+                            allMessages.Add(client.GetMessage(i));
+                        }
+//                        client.Disconnect();
+
+                        foreach (OpenPop.Mime.Message msg in allMessages.ToArray())
+                        {
+                            txtLog.AppendText("Дата: " + msg.Headers.Date.ToString()+ Environment.NewLine);
+                            txtLog.AppendText("Отправитель: " + msg.Headers.From.MailAddress.ToString() + Environment.NewLine);
+                            txtLog.AppendText("Тема: " + msg.Headers.Subject + Environment.NewLine);
+                            txtLog.AppendText("Сообщение: " +( msg.MessagePart.IsText? msg.MessagePart.GetBodyAsText():" 71. multipart ") + Environment.NewLine + Environment.NewLine);
+                        }
 
                     }
                     catch (Exception ex)
                     {
-                        txtLog.AppendText (DateTime.Now.ToLongTimeString() + " 71.Error: " + ex.Message + "\r\n\r\n");
+                        txtLog.AppendText(DateTime.Now.ToLongTimeString() + " 71.Error: " + ex.Message + "\r\n\r\n");
                         txtLog.Update();
                         client.Disconnect();
                         btnConnect.Text = "Connect";
-                        txtLog.AppendText (DateTime.Now.ToLongTimeString() + " 73.Disconnected.\r\n\r\n");
+                        txtLog.AppendText(DateTime.Now.ToLongTimeString() + " 73.Disconnected.\r\n\r\n");
                         return;
                     }
                 }
                 catch (Exception ex)
                 {
-                    txtLog.AppendText (DateTime.Now.ToLongTimeString() + " 79.Error: " + ex.Message + "\r\n\r\n");
+                    txtLog.AppendText(DateTime.Now.ToLongTimeString() + " 79.Error: " + ex.Message + "\r\n\r\n");
                     txtLog.Update();
                     btnConnect.Text = "Connect";
                     return;
@@ -86,7 +94,7 @@ namespace mailagent
             {
                 btnConnect.Text = "Connect";
                 client.Disconnect();
-                txtLog.AppendText (DateTime.Now.ToLongTimeString() + " 89.Disconnected.\r\n\r\n");
+                txtLog.AppendText(DateTime.Now.ToLongTimeString() + " 89.Disconnected.\r\n\r\n");
             }
         }
 
@@ -98,18 +106,18 @@ namespace mailagent
                 try
                 {
                     client.SendCommand(this.mssg_txtbx.Text);
-                    txtLog.AppendText (DateTime.Now.ToLongTimeString() + " 101.Sending :  " + this.mssg_txtbx.Text + ".\r\n\r\n");
-                    txtLog.AppendText (DateTime.Now.ToLongTimeString() + " 102.Getting :  " + client.GetServerResponse() + ".\r\n\r\n");
+                    txtLog.AppendText(DateTime.Now.ToLongTimeString() + " 101.Sending :  " + this.mssg_txtbx.Text + ".\r\n\r\n");
+                    txtLog.AppendText(DateTime.Now.ToLongTimeString() + " 102.Getting :  " + client.GetServerResponse() + ".\r\n\r\n");
                 }
                 catch (Exception ex)
                 {
-                    txtLog.AppendText (DateTime.Now.ToLongTimeString() + " 106.ERROR :" + ex.Message+ ".\r\n\r\n");
+                    txtLog.AppendText(DateTime.Now.ToLongTimeString() + " 106.ERROR :" + ex.Message + ".\r\n\r\n");
                 }
 
-             }
+            }
             else
             {
-                txtLog.AppendText (DateTime.Now.ToLongTimeString() + " 112.No connection.\r\n\r\n");
+                txtLog.AppendText(DateTime.Now.ToLongTimeString() + " 112.No connection.\r\n\r\n");
             }
         }
 
