4a5725cd56ef0953620241641905dc712ecd194d
Oleg <pop457045@mail.ru>
2017-02-22 17:30:40 +0300

    little disign correction

--- mailagent/Form1.Designer.cs
+++ mailagent/Form1.Designer.cs
@@ -47,11 +47,11 @@ namespace mailagent
             // txtLog
             // 
             this.txtLog.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.txtLog.Location = new System.Drawing.Point(0, 147);
+            this.txtLog.Location = new System.Drawing.Point(0, 109);
             this.txtLog.Multiline = true;
             this.txtLog.Name = "txtLog";
             this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtLog.Size = new System.Drawing.Size(427, 423);
+            this.txtLog.Size = new System.Drawing.Size(741, 461);
             this.txtLog.TabIndex = 0;
             this.txtLog.TabStop = false;
             this.txtLog.TextChanged += new System.EventHandler(this.txtLog_TextChanged);
@@ -66,9 +66,9 @@ namespace mailagent
             // 
             // txtPass
             // 
-            this.txtPass.Location = new System.Drawing.Point(298, 36);
+            this.txtPass.Location = new System.Drawing.Point(481, 36);
             this.txtPass.Name = "txtPass";
-            this.txtPass.Size = new System.Drawing.Size(115, 20);
+            this.txtPass.Size = new System.Drawing.Size(157, 20);
             this.txtPass.TabIndex = 4;
             // 
             // label1
@@ -83,7 +83,7 @@ namespace mailagent
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(236, 13);
+            this.label2.Location = new System.Drawing.Point(263, 13);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(29, 13);
             this.label2.TabIndex = 5;
@@ -92,7 +92,7 @@ namespace mailagent
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(26, 43);
+            this.label3.Location = new System.Drawing.Point(414, 8);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(58, 13);
             this.label3.TabIndex = 6;
@@ -101,7 +101,7 @@ namespace mailagent
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(236, 39);
+            this.label4.Location = new System.Drawing.Point(419, 36);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(56, 13);
             this.label4.TabIndex = 7;
@@ -109,23 +109,23 @@ namespace mailagent
             // 
             // txtUser
             // 
-            this.txtUser.Location = new System.Drawing.Point(90, 36);
+            this.txtUser.Location = new System.Drawing.Point(478, 5);
             this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(125, 20);
+            this.txtUser.Size = new System.Drawing.Size(160, 20);
             this.txtUser.TabIndex = 3;
             // 
             // txtServer
             // 
             this.txtServer.Location = new System.Drawing.Point(90, 10);
             this.txtServer.Name = "txtServer";
-            this.txtServer.Size = new System.Drawing.Size(125, 20);
+            this.txtServer.Size = new System.Drawing.Size(167, 20);
             this.txtServer.TabIndex = 1;
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(328, 62);
+            this.btnConnect.Location = new System.Drawing.Point(644, 5);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(85, 28);
+            this.btnConnect.Size = new System.Drawing.Size(85, 51);
             this.btnConnect.TabIndex = 6;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = true;
@@ -134,7 +134,7 @@ namespace mailagent
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 119);
+            this.label5.Location = new System.Drawing.Point(12, 93);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(28, 13);
             this.label5.TabIndex = 11;
@@ -154,16 +154,16 @@ namespace mailagent
             // 
             // mssg_txtbx
             // 
-            this.mssg_txtbx.Location = new System.Drawing.Point(15, 62);
+            this.mssg_txtbx.Location = new System.Drawing.Point(15, 36);
             this.mssg_txtbx.Multiline = true;
             this.mssg_txtbx.Name = "mssg_txtbx";
-            this.mssg_txtbx.Size = new System.Drawing.Size(299, 54);
+            this.mssg_txtbx.Size = new System.Drawing.Size(393, 54);
             this.mssg_txtbx.TabIndex = 7;
             this.mssg_txtbx.Text = "mssg to send";
             // 
             // mssg_bttn
             // 
-            this.mssg_bttn.Location = new System.Drawing.Point(328, 96);
+            this.mssg_bttn.Location = new System.Drawing.Point(422, 62);
             this.mssg_bttn.Name = "mssg_bttn";
             this.mssg_bttn.Size = new System.Drawing.Size(85, 28);
             this.mssg_bttn.TabIndex = 8;
@@ -175,7 +175,7 @@ namespace mailagent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(427, 570);
+            this.ClientSize = new System.Drawing.Size(741, 570);
             this.Controls.Add(this.mssg_bttn);
             this.Controls.Add(this.mssg_txtbx);
             this.Controls.Add(this.SSL_bx);
